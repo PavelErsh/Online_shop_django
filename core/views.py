717477f4c05e74b1d79f340603640e921b05f66a
PaverErsh <pavel204812@gmail.com>
2022-07-12 01:09:21 +0200

    add style and base

--- core/views.py
+++ core/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'main.html')
+    var = "hello"
+    return render(request, 'main.html', {'var':var})
